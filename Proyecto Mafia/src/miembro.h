91096fe017a7a461b764206f9e7ad4ed2eb4c867
Jesuslperezc <jesusleonardoperezcabrera@gmail.com>
2026-04-14 16:30:42 -0400

    feat: agregar constructores y métodos de estado a miembro

--- Proyecto Mafia/src/miembro.h
+++ Proyecto Mafia/src/miembro.h
@@ -17,5 +17,21 @@ struct Miembro {
     Miembro* izq;
     Miembro* der;
     Miembro* padre;
+    
+    // Constructor por defecto 
+    Miembro() : id(0), gender(' '), age(0), id_boss(0), is_dead(false), 
+                in_jail(false), was_boss(false), is_boss(false), 
+                izq(nullptr), der(nullptr), padre(nullptr) {}
+
+    // Constructor completo
+    Miembro(int id, const std::string& n, const std::string& ln, char g, int a, 
+            int idb, bool dead, bool jail, bool wboss, bool iboss)
+        : id(id), name(n), last_name(ln), gender(g), age(a), id_boss(idb),
+          is_dead(dead), in_jail(jail), was_boss(wboss), is_boss(iboss),
+          izq(nullptr), der(nullptr), padre(nullptr) {}
+
+    bool esta_vivo() const { return !is_dead; }
+    bool esta_libre() const { return !is_dead && !in_jail; }
 };
+
 #endif

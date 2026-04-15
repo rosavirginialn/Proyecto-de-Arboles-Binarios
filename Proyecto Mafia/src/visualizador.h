fdaa49ab05393ff8069044dca359d50389eda807
Jesuslperezc <jesusleonardoperezcabrera@gmail.com>
2026-04-15 19:09:33 -0400

    feat: definir funciones de visualización de arbol y sus miembros

---
+++ Proyecto Mafia/src/visualizador.h
@@ -0,0 +1,14 @@
+#ifndef VISUALIZADOR_HPP
+#define VISUALIZADOR_HPP
+
+#include "arbol_mafia.h"
+#include <iostream>
+#include <string>
+
+
+void imprimir_arbol(Miembro* nodo, int nivel = 0);
+
+
+void mostrar_sucesion_viva(Miembro* nodo);
+
+#endif
